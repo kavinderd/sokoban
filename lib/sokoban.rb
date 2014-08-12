02a1db7d928e98d2d06fc129fd5aaa4150da4099
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-08-12 07:22:49 -0700

    nasty completed prototype of sokoban

--- lib/sokoban.rb
+++ lib/sokoban.rb
@@ -5,13 +5,14 @@ module Sokoban
 	class Level
 		UP    = [-1,0]
 		DOWN  = [1,0]
-		LEFT  = [0, -1]
+		LEFT  = [0,-1]
 		RIGHT = [0,1]
 		NONE  = [0,0]
 
 		attr_reader :grid
 		def initialize(level_string)
 			@grid = level_string.split("\n").map{|line| line.split(//).map{|char| Tile.create(char)}}
+			@move_count = 0
 		end
 
 		def to_s
@@ -21,11 +22,24 @@ module Sokoban
 		def player_position
 			@grid.each_index do |row|
 			  @grid[row].each_index do |col|
-					if @grid[row][col].type == :player || @grid[row][col] == :player_on_storage
+					if @grid[row][col].type == :player || @grid[row][col].type == :player_on_storage
 						return [row, col]
 					end
 				end
 		  end
+		end
+
+
+		def crate_count
+			@crate_count = 0
+		  @grid.each_index do |row|
+				@grid[row].each_index do |col|
+					if @grid[row][col].type == :crate
+						@crate_count += 1
+					end
+				end
+			end
+			@crate_count
 		end
 
 		def [](row, col)
@@ -41,6 +55,8 @@ module Sokoban
 			@@levels
 	  end
 		
+		#TODO: CLEAN THIS UP
+		#TODO: COVER CASE WHEN PLAYER MOVES OFF STORAGE ONTO FREE POSITION
 		def move(movement)
       move = case movement
 					   when 'UP'
@@ -54,21 +70,66 @@ module Sokoban
 						 else
 							 NONE
 						 end
-			puts move
 			player = player_position
 			move_position = player[0] + move[0], player[1] + move[1]
-			puts move_position
-			puts player
 			final_position= @grid[move_position[0]][move_position[1]]
-			puts final_position
+			player_tile  = @grid[player[0]][player[1]]
 			unless final_position.nil? || final_position.type == :invalid
 				if final_position.type == :floor
-					puts @grid
-					@grid[move_position[0]][move_position[1]], @grid[player[0]][player[1]] = @grid[player[0]][player[1]], @grid[move_position[0]][move_position[1]]
-					puts @grid
+					if player_tile.type == :player_on_storage
+					  @grid[move_position[0]][move_position[1]], @grid[player[0]][player[1]] = Tile.create("@"), Tile.create(".")	
+					else
+					  @grid[move_position[0]][move_position[1]], @grid[player[0]][player[1]] = @grid[player[0]][player[1]], @grid[move_position[0]][move_position[1]]
+					end
+				elsif final_position.type == :storage
+					player_tile = @grid[player[0]][player[1]]
+					if player_tile.type == :player_on_storage
+						@grid[move_position[0]][move_position[1]], @grid[player[0]][player[1]] = Tile.create("+"), Tile.create(".")
+					else
+					  @grid[move_position[0]][move_position[1]], @grid[player[0]][player[1]] = Tile.create('+'), Tile.create(" ")
+					end
+				elsif final_position.type == :crate
+					next_to_final_position = move_position[0] + move[0], move_position[1] + move[1]
+					next_to_final_tile = @grid[next_to_final_position[0]][next_to_final_position[1]]
+					stack = []
+					if next_to_final_tile.type == :floor
+						stack = [Tile.create('o'), Tile.create('@'), Tile.create(' ')]
+					elsif next_to_final_tile.type == :storage
+						stack = [Tile.create('*'), Tile.create('@'), Tile.create(' ')]
+					elsif next_to_final_tile.type == :crate || next_to_final_tile.type == :wall
+						stack = [Tile.create(next_to_final_tile.to_s), Tile.create('o'), Tile.create('@')]
+					end
+						@grid[player[0]][player[1]] = stack.pop
+						@grid[move_position[0]][move_position[1]] = stack.pop
+						@grid[next_to_final_position[0]][next_to_final_position[1]] = stack.pop
+				elsif final_position.type == :crate_on_storage
+			    next_to_final_position = move_position[0] + move[0], move_position[1] + move[1]
+					next_to_final_tile = @grid[next_to_final_position[0]][next_to_final_position[1]]
+					stack = []
+					player_tile = @grid[player[0]][player[1]]
+					if next_to_final_tile.type == :free
+						if player.type == :player_on_storage
+							stack = [Tile.create("o"), Tile.create("+"), Tile.create(".")]
+						else
+							stack = Tile.create("o"), Tile.create("+"), Tile.create(" ")
+						end
+					elsif next_to_final_tile.type == :storage
+						if player_tile.type == :player_on_storage
+							stack = Tile.create("*"), Tile.create("+"), Tile.create(".")
+						else 
+							stack = Tile.create("*"), Tile.create("+"), Tile.create(" ")
+						end
+					elsif next_to_final_title.type == :wall || next_to_final_tile.type == :crate
+				   	stack = [Tile.create(next_to_final_tile.to_s), Tile.create('*'), Tile.create('@')]
+					end
+						@grid[player[0]][player[1]] = stack.pop
+						@grid[move_position[0]][move_position[1]] = stack.pop
+						@grid[next_to_final_position[0]][next_to_final_position[1]] = stack.pop
 				else
+					@move_count -= 1
 				end
 	    end
+			@move_count += 1
 			puts to_s
 		end
 
@@ -101,7 +162,7 @@ module Sokoban
 				:player
 			when "o"
 				:crate
-			when "-"
+			when "."
 				:storage
 			when "+"
 				:player_on_storage
---
+++ test/move_over_storage.rb
@@ -0,0 +1,19 @@
+$:.unshift('.')
+$:.unshift("./lib")
+$:.unshift('..')
+$:.unshift('../lib')
+
+require 'sokoban'
+
+s = Sokoban::Level.parse_levels('sokoban_levels.txt')
+level =s.first
+level.move("UP")
+level.move("RIGHT")
+level.move("RIGHT")
+level.move("RIGHT")
+level.move("RIGHT")
+level.move("RIGHT")
+level.move("RIGHT")
+
+
+
